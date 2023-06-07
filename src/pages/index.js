32cfd537846668215b3f23afff706aeaeac6ac77
ariffsulaiman <105694846+ariffsulaiman@users.noreply.github.com>
2023-06-07 21:22:32 +0800

    Add files via upload

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
   return (
     <div className='flex flex-col my-20'>
 
-      <section className="flex flex-col md:flex-row md:justify-center md:items-center md:-space-x-4 mx-auto">
+      <section className="flex flex-col md:flex-row md:justify-center md:items-center md:-space-x-8 mx-auto">
         <Image
           src="/fotor_2023-3-17_15_50_31.png"
           alt=""
@@ -23,7 +23,7 @@ export default function Home() {
           className="z-10 w-60 rounded-full ring-8 ring-red-500 ring-offset-4 mx-auto md:mx-0"
           priority={true}/>
 
-        <div className="bg-red-500 w-[90vw] md:w-[39vw] mx-auto md:mx-0 py-4 px-4 md:pt-12 md:pr-6 mt-6 md:mt-0 rounded-md md:rounded-none">
+        <div className="bg-red-500 w-[90vw] md:w-[43vw] mx-auto md:mx-0 py-4 px-4 md:pt-12 md:pr-6 mt-6 md:mt-0 rounded-md md:rounded-none">
           <div className="text-center md:text-right font-bold md:text-3xl">
             MUHAMAD ARIFF BIN SULAIMAN
           </div>
